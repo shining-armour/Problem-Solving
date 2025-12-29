bbbecce43c3338d74113e3555a1553842b6d53db
Rafat Ansari <46320002+shining-armour@users.noreply.github.com>
2025-12-29 12:47:45 +0530

    Time: 7 ms (23.93%), Space: 59.7 MB (6.55%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.kt
+++ 0344-reverse-string/0344-reverse-string.kt
@@ -1,5 +1,21 @@
 class Solution {
     fun reverseString(s: CharArray): Unit {
-        println(s.reverse())
+        val stack = Stack<Char>()
+        //val result = mutableListOf<Char>()
+        
+        for (ch in s) {
+          stack.push(ch)
+        }
+
+       // No in-place reversal 
+       //while (stack.isNotEmpty()) {
+        //result.add(stack.pop())
+       //}
+
+       var i = 0
+       while (stack.isNotEmpty()) {
+           s[i] = stack.pop()  
+           i++ 
+        }
     }
 }
